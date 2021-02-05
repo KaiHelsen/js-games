b224b5b1eef5e9cc6d783847cfd93f227ead0f18
KaiHelsen <kai@Helsen.info>
2021-02-05 11:35:57 +0100

    updated physics model to work with drag instead of fake deceleration, started work on control point mechanic and general cleanup

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@
 {
     //define constant values for gameplay
     const PLAYER_SPEED = 1200; //units per second
-    const PLAYER_ROTATION_SPEED = 30; //degrees per second
     const FPS = 60;
     const DELTA_TIME = 1 / FPS;
     const MAX_LIVES = 3;
@@ -35,12 +34,18 @@
 
     /**
      *
-     * @param {Vector} otherVector
+     * @param Vector1
+     * @param Vector2
+     * @returns {Vector}
      */
-    Vector.prototype.add = function (otherVector)
+    Vector.add = function (Vector1, Vector2)
     {
-        this.x += otherVector.x;
-        this.y += otherVector.y;
+        return new Vector(Vector1.x + Vector2.x, Vector1.y + Vector2.y);
+    }
+
+    Vector.subtract = function(Vector1, Vector2)
+    {
+        return new Vector(Vector1.x - Vector2.x, Vector1.y - Vector2.y);
     }
 
     /**
@@ -73,6 +78,9 @@
         this.multiply(newMagnitude);
     }
 
+    /**
+     * invert Vector direction
+     */
     Vector.prototype.invert = function ()
     {
         this.x *= -1;
@@ -80,6 +88,16 @@
     }
 
     //rect and rect functions
+    /**
+     * Rect functions handle and support rectangular shapes and collision checks between them
+     * @param {number} x x coordinate of the top left corner of the rect
+     * @param {number} y y coordinate of the top left corner of the rect
+     * @param {number} height height of the rect
+     * @param {number} width width of the rect
+     * @param {number} posX optional, x position in world space of the rect. the rects center point is placed at this position.
+     * @param {number} posY optional, y position in world space of the rect. the rects center point is placed at this position.
+     * @constructor
+     */
     let Rect = function (x = 0, y = 0, height = 0, width = 0, posX = 0, posY = 0)
     {
         this.position = new Vector(x, y);
@@ -89,6 +107,11 @@
         this.setPosition(posX, posY);
     }
 
+    /**
+     * set position of the rect. the position is placed according to its center point, rather than its top left corner
+     * @param xPos
+     * @param yPos
+     */
     Rect.prototype.setPosition = function (xPos = this.center.x, yPos = this.center.y)
     {
         this.center.x = xPos;
@@ -99,6 +122,11 @@
 
     }
 
+    /**
+     * adds input coordinates to rects current coordinates.
+     * @param x
+     * @param y
+     */
     Rect.prototype.move = function (x = 0, y = 0)
     {
         this.center.x += x;
@@ -108,7 +136,13 @@
         this.position.y = this.center.y - this.size.y / 2;
     }
 
-    Rect.prototype.checkCollision = function (otherRect)
+    /**
+     * Returns true if this rect and the input rect overlap.
+     * TODO: make a function to calculate overlap and return as a vector to allow for adjustment?
+     * @param otherRect
+     * @returns {boolean}
+     */
+    Rect.prototype.checkCollisionRect = function (otherRect)
     {
         return (
             this.position.x < otherRect.position.x + otherRect.size.x &&
@@ -117,6 +151,29 @@
             this.position.y + this.size.y > otherRect.position.y);
     }
 
+    let Circle = function(x = 0, y = 0, radius = 0, posX = 0, posY = 0)
+    {
+        this.position = new Vector(x,y);
+        this.radius = radius;
+        this.diameter = radius * 2;
+        this.center = new Vector(x + radius, y + radius);
+    }
+
+    Circle.prototype.setPosition = function(xPos = this.center.x, yPos = this.center.y)
+    {
+        this.center.x = xPos;
+        this.center.y = yPos;
+
+        this.position.x = this.center.x - this.radius;
+        this.position.y = this.center.y - this.radius;
+    }
+
+    /**
+     * returns true if this rect is within the bounds of the input rect
+     * TODO: make a function to calculate overlap and return as a vector to allow for adjustment?
+     * @param bounds
+     * @returns {boolean}
+     */
     Rect.prototype.withinBounds = function (bounds = new Rect())
     {
         return (
@@ -127,18 +184,26 @@
         )
     }
 
-    //block class
+    /**
+     * block class is a basic class to handle the moving blocks in the game. every block requires an element to properly display in the viewport
+     */
     class Block
     {
-        momentum = new Vector(0, 0);
         element;                //display element
         name = "myName";        //name of this object
         collider;               //collider rect of this object
         rotation;               //stores rotation of this object
-        acceleration = 60;
-        deceleration = 8;
-        isActive = true;
+        isActive = true;        //whether or not this element is "active" within the world/game and should be accounted for
 
+        /**
+         * The block class is the basic element that handles in-game block elements. player, enemies, obstacles, etc.
+         * @param {HTMLElement} element HTML element that is displayed in-game.
+         * @param {string} name name of this element
+         * @param {number} height height of this element's rect
+         * @param {number} width width of this element's rect
+         * @param {number} posX X position of the element's center point.
+         * @param {number} posY Y position of the elmeent's center point.
+         */
         constructor(element, name = "myName", height = 20, width = 20, posX = 0, posY = 0) {
             this.element = element;
             this.name = name;
@@ -149,72 +214,12 @@
             this.draw();
         };
 
-        /**
-         *
-         * @param {Vector} direction direction vector in which the block should move
-         */
-        move(direction) {
-            if (direction.magnitude() > 0 && direction) {
-                direction.normalize();
-                // direction.multiply(PLAYER_SPEED * DELTA_TIME);
-                direction.multiply(this.acceleration * DELTA_TIME);
-
-                // console.log(direction);
-                this.momentum.add(direction);
-                this.momentum.clamp(PLAYER_SPEED * DELTA_TIME);
-
-            }
-            else
-                if(this.momentum.magnitude() >= this.deceleration * DELTA_TIME * .2)
-            {
-                let deceleration = new Vector(this.momentum.x, this.momentum.y);
-                deceleration.normalize();
-
-                deceleration.invert();
-                deceleration.multiply(this.deceleration * DELTA_TIME);
-
-                // console.log(deceleration)
-                this.momentum.add(deceleration);
-
-            }
-            else {
-                this.momentum.multiply(0);
-            }
-
-
-            this.collider.move(this.momentum.x, this.momentum.y);
-        }
-
         collisionCheck(otherRect) {
-            if (this.collider.checkCollision(otherRect)) {
-                return true;
-            }
-            else {
-                return false;
-            }
+            return this.collider.checkCollisionRect(otherRect);
         }
 
-        forceGameBounds() {
-            //clamp to window
-            let xRange = window.innerWidth / 2;
-            let yRange = window.innerHeight / 2;
-            let screenCenter = new Vector(xRange, yRange);
-
-            //clamp within bounds
-            this.collider.center.x = Math.max(this.collider.size.x / 2, Math.min(this.collider.center.x, window.innerWidth - this.collider.size.x / 2));
-            this.collider.center.y = Math.max(this.collider.size.y / 2, Math.min(this.collider.center.y, window.innerHeight - this.collider.size.y / 2));
-
-            if(this.collider.position.x <= 0 || this.collider.position.x + this.collider.size.x >= window.innerWidth)
-            {
-                this.momentum.x = this.momentum.x * -.3;
-            }
-            if(this.collider.position.y <= 0 || this.collider.position.y + this.collider.size.y >= window.innerHeight)
-            {
-                this.momentum.y = this.momentum.y * -.3;
-            }
-
-
-            //limit momentum
+        update(){
+            this.draw();
         }
 
         draw() {
@@ -229,21 +234,102 @@
             this.element.style.width = this.collider.size.y + "px";
             this.element.style.height = this.collider.size.x + "px";
 
+            if(!this.isActive){this.element.style.visibility = "hidden"}
+            else{this.element.style.visibility = "visible"}
+
         }
 
         forcePosition(posX, posY) {
             this.collider.center.x = posX;
             this.collider.center.y = posY;
         }
-
-        deSpawn() {
-            this.element.style.visibility = "hidden";
-            this.element.isMoving = false;
-            this.isActive = false;
-            this.forcePosition(0, 0);
-        }
     }
 
+    /**
+     * Player Controller is an extension of the block class that handles methods specific to the player.
+     * because of the way it moves, it has an internal simplified physics system
+     */
+    class PlayerController extends Block
+    {
+        momentum = new Vector(0, 0); //current momentum of the player controller
+        maxSpeed = 0; //maximum speed this object can reach
+        acceleration = 0; //rate at which this object accelerates. ie. this is the amount by which the speed increases per second
+        drag = 0; //slows down object movement gently
+
+
+        constructor(element, name, height, width, posX, posY, maxSpeed, acceleration, drag) {
+            super(element, name, height, width, posX, posY);
+            this.maxSpeed = maxSpeed;
+            this.acceleration = acceleration;
+            this.drag = drag;
+        }
+
+        update(){
+            move(inputVector)
+
+            this.draw()
+        }
+        /**
+         * Based on input direction vector, this function moves the Player Controller based on simple physics
+         * first, evaluate whether there is any input at all. If there is, the object is accelerated in the direction of the input
+         * second, if the momentum of the object is above a minimum threshold, drag is added to the momentum, ensuring the controller slows down.
+         * last, if the momentum of the object is BELOW the minimum threshold, the momentum is set to zero.
+         * finally, the collider is moved based on the resulting momentum.
+         * @param {Vector} direction direction vector in which forces are being applied to the Player Controller
+         */
+        move(direction) {
+            if (direction.magnitude() > 0 && direction){
+                direction.normalize();
+
+                direction.multiply(this.acceleration * DELTA_TIME);
+
+                this.momentum = Vector.add(this.momentum, direction);
+            }
+
+            if(Math.abs(this.momentum.x) >= this.drag * DELTA_TIME || Math.abs(this.momentum.y) >= this.drag * DELTA_TIME)
+            {
+                //add friction deceleration if object is NOT at rest.
+                let velocity = this.momentum.magnitude();
+                let friction = new Vector(this.momentum.x, this.momentum.y);
+                friction.normalize();
+                friction.multiply(-1 * this.drag * velocity * DELTA_TIME);
+
+                this.momentum = Vector.add(this.momentum, friction);
+            }
+            else{
+                this.momentum.multiply(0);
+            }
+
+            this.collider.move(this.momentum.x, this.momentum.y);
+        }
+
+        /**
+         * forces Player controller to stay within the bounds of the playable space
+         * If the Player controller hits the border with a certain momentum, they will bounce back.
+         * Additionally, the position of the Player Controller will also be clamped inside the bounds of the screen to ensure they cannot leave at all.
+         */
+        forceGameBounds() {
+
+            //clamp within bounds
+            this.collider.center.x = Math.max(this.collider.size.x / 2, Math.min(this.collider.center.x, screen.x - this.collider.size.x / 2));
+            this.collider.center.y = Math.max(this.collider.size.y / 2, Math.min(this.collider.center.y, screen.y - this.collider.size.y / 2));
+
+            if (this.collider.position.x <= 0 || this.collider.position.x + this.collider.size.x >= screen.x) {
+                this.momentum.x = this.momentum.x * -.3;
+            }
+            if (this.collider.position.y <= 0 || this.collider.position.y + this.collider.size.y >= screen.y) {
+                this.momentum.y = this.momentum.y * -.3;
+            }
+
+
+            //limit momentum
+        }
+
+    }
+
+    /**
+     * Enemy Block is an extension of the block class that handles methods specific to the standard enemy.
+     */
     class EnemyBlock extends Block
     {
         maxSpeed = 0; //maximum speed at which this unit moves;
@@ -306,8 +392,8 @@
         pickDestination() {
             let padding = new Vector(this.collider.size.x, this.collider.size.y);
 
-            let newTargetX = Math.random() * (window.innerWidth - padding.x) + padding.x / 2 | 0;
-            let newTargetY = Math.random() * (window.innerHeight - padding.y) + padding.y / 2 | 0;
+            let newTargetX = Math.random() * (screen.x - padding.x) + padding.x / 2 | 0;
+            let newTargetY = Math.random() * (screen.y - padding.y) + padding.y / 2 | 0;
 
             this.setTargetPosition(newTargetX, newTargetY);
 
@@ -316,16 +402,49 @@
 
         spawn(posX = 0, posY = 0) {
             this.element.isMoving = true;
-            this.isActive = true;
             this.forcePosition(posX, posY);
             this.pickDestination();
 
-            setTimeout(() => {this.element.style.visibility = "visible"}, .1 * 1000);
+            setTimeout(() => {this.isActive = true}, .1 * 1000);
         };
+
+        deSpawn() {
+            this.element.isMoving = false;
+            this.isActive = false;
+            this.forcePosition(0, 0);
+        }
 
     }
 
-    class EnemySpawner
+    /**
+     * Control Point defines and handles the control point element
+     * The control point grants the player points as long as the center of their block is within the radius of the control point
+     * The control point should be spawned at progressively smaller sizes, and closer to the center at regular intervals
+     * This will motivate the player to move around and not just sit around in a corner, waiting.
+     */
+    class ControlPoint extends Block
+    {
+        radius = 0
+
+        constructor(element, name = "myName", radius = 0, posX = 0, posY = 0)
+        {
+            super(element, name, radius * 2, radius * 2, posX, posY);
+            this.radius = radius;
+            this.isActive = false;
+
+            this.draw();
+        }
+    }
+    /**
+     * Enemy Manager is built to handle enemies easily. It's functions include (but are not limited to):
+     * -spawning new enemies
+     * -despawning enemies when they hit the player or are otherwise "killed"
+     * -re-spawning enemies that have been despawned or killed
+     * -keeping track of enemies
+     * -checking collision of ALL enemies with another rect or block
+     * -limiting the amount of enemies in the game if appropriate
+     */
+    class EnemyManager
     {
         constructor(maxEnemiesInPlay, initialDelay, spawnDelayMin, spawnDelayMax) {
             this.enemiesList = [];
@@ -339,7 +458,6 @@
             this.enemiesList.push(newEnemy);
             let enemy = this.enemiesList[this.enemiesList.length - 1];
             enemy.isActive = isActive;
-            enemy.element.style.visibility = "hidden";
         }
 
         spawnEnemy() {
@@ -349,7 +467,7 @@
                 //pick random location outside of window to spawn enemy
                 //let's be lazy and just have them move in from the top of the screen for now
 
-                let posX = Math.random() * window.innerWidth;
+                let posX = Math.random() * screen.x;
                 let posY = Math.random() * -200;
 
                 // console.log("Spawning new enemy!: " + enemy.name + " at X: " + posX + " Y: " + posY);
@@ -387,6 +505,12 @@
 
     }
 
+    /**
+     * Game Manager oversees all other functions and handles the "gameplay systems" as it were.
+     * Game manager handles score, the general timer of the game, and keeps track of the game state.
+     * TODO: build in scheduling system, that allows the user to schedule certain events
+     *  -ie. spawning a certain rect at a specific point in time
+     */
     class GameManager
     {
         constructor(playerLives, livesDisplay, scoreDisplay, gameplayDisplay, gameOverDisplay) {
@@ -396,12 +520,16 @@
             this.gameDsp = gameplayDisplay
             this.gameOverDsp = gameOverDisplay
             this.timer = 0;
+
+            this.score = 0;
+
+            this.scheduler = [];
         };
 
-        startGame(onStartFunction = new Function()) {
+        startGame(onStart = new Function()) {
             this.gameDsp.style.visibility = "visible";
             this.gameOverDsp.style.visibility = "hidden";
-            onStartFunction();
+            onStart();
         }
 
         endGame() {
@@ -429,10 +557,14 @@
                 this.endGame();
             }
         }
+
+        schedule(callbackFunction, delay, repeats){
+
+        };
     }
 
     console.log("initializing...");
-    let screenSize = {width: window.innerWidth, height: window.innerHeight};
+    let screen = new Vector(window.innerWidth,window.innerHeight);
 
     let game = new GameManager(
         MAX_LIVES,
@@ -442,13 +574,25 @@
         document.getElementById("gameOverScreen")
     );
 
-    let playerCube = new Block(document.getElementById("player"), "player", 80, 80, screenSize.width / 2, screenSize.height / 2);
+    let playerCube = new PlayerController(
+        document.getElementById("player"),
+        "player",
+        80,
+        80,
+        screen.x / 2,
+        screen.y / 2,
+        PLAYER_SPEED,
+        60,
+        1.2);
 
-    let spawner = new EnemySpawner(MAX_ENEMIES, 3, 3, 6);
-
-    // let enemyCube = new EnemyBlock(document.getElementById("enemy_01"), "enemy", 90, 90, 120, 120, 300);
+    //initialize classes
+    let spawner = new EnemyManager(MAX_ENEMIES, 3, 3, 6);
 
     let enemyCubeTemplate = document.getElementById("enemyTemplate").content.cloneNode(true);
+
+    let controlPointTemplate = document.getElementById("controlPointTemplate").content.cloneNode(true);
+
+    let controlPoint = new ControlPoint(document.querySelector(".controlPoint"), "Control Point", 400, screen.x/2, screen.y/2);
 
     for (let i = 0; i < MAX_ENEMIES; i++) {
         //create a series of enemies
@@ -465,18 +609,24 @@
         spawner.addEnemyToList(new EnemyBlock(document.getElementById(newId), newName, 60 + i * 5, 60 + i * 5, 120, 120, 300), false);
     }
 
-    //spawn first enemy for testing purposes
-    // spawner.spawnEnemy();
 
-
+    //initialize input management
     let input = {up: false, down: false, left: false, right: false};
 
     document.addEventListener("keydown", keyDownHandler, false);
 
     document.addEventListener("keyup", keyUpHandler, false);
 
+    document.addEventListener("resize",()=>{
+        screen.x = window.innerWidth;
+        screen.y = window.innerHeight;
+    }, false);
+
+    //initialize gameTimer
     let gameTimer = setInterval(tick, DELTA_TIME * 1000);
 
+
+    //START GAME
     game.startGame();
     spawner.start();
 
